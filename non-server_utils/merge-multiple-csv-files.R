a26d65b3b89f0b437fd31ffd9892c7b8a78e8f55
Aliaksandr Halavatyi <aliaksandr.halavatyi@embl.de>
2018-02-21 13:02:42 +0100

    minor changes in merging tool

--- non-server_utils/merge-multiple-csv-files.R
+++ non-server_utils/merge-multiple-csv-files.R
@@ -12,6 +12,12 @@ if (!require(plyr)) install.packages('plyr')
 
 rm(list=ls())#clear all variables
 
+inputPath<-'C:/tempDat/Tischi/small-test-image-sequences--analysis--20180221'
+outputFileName<-'merged_table.csv'
+dataset.Pattern<-'(?<Well>\\w[0-9]{1,2})--W(?<WellNum>[0-9]{5})--P(?<Position>[0-9]{5})--Z--T'
+fileNamePattern<-'\\w[0-9]{1,2}--W[0-9]{5}--P[0-9]{5}--Z--T--(?<Suffix>.*)\\.csv'#
+
+
 suffixes.Dataset.Tables<-c('ApplyClassifier')
 suffixes.Object.Tables<-c('foreground.tif--Volume--AnalyzeObjects','foreground.tif--BoundingBox--AnalyzeObjects','foreground.tif--Ellipsoid--AnalyzeObjects')
 
@@ -19,16 +25,9 @@ columnLabel.Dataset<-'DataSetID_FACT'
 columnLabel.Object<-'Label'
 
 
-inputPath<-'D:/tempDat/Tischi/small-test-image-sequences--analysis--20180221'
-outputFileName<-'merged_table.csv'
-#inputNameSuperPattern<-'(?<Prefix>.*)%s.csv' #can be pattern rather than name
-#PrefixNamePattern<-'(?<Well>\\w[0-9]{1,2})--W(?<WellNum>[0-9]{5})--P(?<Position>[0-9]{5})--Z--T--'
-dataset.Pattern<-'(?<Well>\\w[0-9]{1,2})--W(?<WellNum>[0-9]{5})--P(?<Position>[0-9]{5})--Z--T'
-fileNamePattern<-'\\w[0-9]{1,2}--W[0-9]{5}--P[0-9]{5}--Z--T--(?<Suffix>.*)\\.csv'#
-
-
 columnSeparator<-','
 decimalSeparator<-'.'
+skip.Input.First.Column<-TRUE
 
 cat ('Start CSV merging','\n')
 setwd(inputPath)
@@ -64,39 +63,21 @@ extractPatternGroups<-function(inputTable,pattern,compColumnName){
 
 input.Files<-extractPatternGroups(inputTable = input.Files,pattern = fileNamePattern,compColumnName = 'FileName')
 
-# for (fileIndex in 1:nFiles){
-#     match<-regexpr(fileNamePattern,input.Files$FileName[fileIndex],perl = TRUE)
-#     matched.groups<-attr(match,'capture.name')
-#     #cat(matched.groups)
-#     for(groupName in matched.groups){
-#         if (!(groupName %in% names(input.Files))){
-#             input.Files[,groupName]<-NA
-#         }
-#         input.Files[fileIndex,groupName]<-substr(input.Files$FileName[fileIndex], 
-#                                                  attr(match, 'capture.start')[,groupName],
-#                                                  attr(match, 'capture.start')[,groupName]
-#                                                  + attr(match, 'capture.length')[,groupName]
-#                                                  - 1)
-#     }
-#     
-# }
-# 
 #combine separately tables of different types
 
 bindTableLines<-function(fileSubPathes){
     result<-data.frame(stringsAsFactors = FALSE)
     for (subPath in fileSubPathes){
-        #kk<-read.csv(subPath,as.is = T)
-        result<-rbind.fill(result,read.csv(subPath,as.is = T,dec = decimalSeparator,sep = columnSeparator))
+        imported.Table<-read.csv(subPath,as.is = T,dec = decimalSeparator,sep = columnSeparator)
+        if (skip.Input.First.Column)
+            imported.Table<-imported.Table[-1]
+        result<-rbind.fill(result,imported.Table)
     }
     return(result);
 }
 
 table.List<-list()
 for (suffix in c(suffixes.Dataset.Tables,suffixes.Object.Tables)){
-    #bb<-input.Files$SubPath[input.Files$Suffix==suffix]
-    #cat(suffix)
-    #aa<-bindTableLines(input.Files$SubPath[input.Files$Suffix==suffix])
     table.List[[suffix]]<-bindTableLines(input.Files$SubPath[input.Files$Suffix==suffix])
 }
 
@@ -119,28 +100,6 @@ for (suffix in suffixes.Dataset.Tables){
     
 }
 
-
-# # Combine files into one big table
-# Merged.Data<-data.frame()
-# library(plyr)
-# #library(stringr)
-# for (input.File in input.Files){
-#     cat ('Procssing file: ')
-#     cat (input.File,'\n')
-#     Rep.Data<-read.csv(input.File,as.is = T)
-#     Rep.Data$FileName_DatasetTable<-basename(input.File)
-#     dataset.Subfolder<-dirname(input.File)
-#     Rep.Data$PathName_DatasetTable<-file.path('root',dataset.Subfolder)
-#     Rep.Match<-regexpr(subfolderNamePattern,'DataSet--B4--W00016--P00004--Z--T--',perl = T)
-#     for(groupName in attr(Rep.Match,'capture.name')){
-#         Rep.Data[groupName]<-substr(dataset.Subfolder, 
-#                                     attr(Rep.Match, 'capture.start')[,groupName],
-#                                     attr(Rep.Match, 'capture.start')[,groupName]
-#                                     + attr(Rep.Match, 'capture.length')[,groupName]
-#                                     - 1)
-#     }
-#     Merged.Data<-rbind.fill(Merged.Data,Rep.Data)
-# }
 # 
 # Save output table
 write.csv(Merged.Data, file=outputFileName,row.names = FALSE,quote = FALSE)
